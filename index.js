7914ccd5754afd0900203e9c135099a9035f3b13
mahdi ali <127436608+mahdi-ali-flutter@users.noreply.github.com>
2023-09-08 23:49:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,53 +6,160 @@ const app=express();
 const params=require("params");
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, useNewUrlParser : true }));
 app.use(express.json());
-
+app.use(bodyParser.json({limit:1024*1024*20}));
+app.use(bodyParser.urlencoded({extended:true,limit:1024*1024*20 }));
 var url = "mongodb://127.0.0.1:27017/notes";
 var url1="mongodb+srv://mahdi:mahdi@cluster0.3lvnvig.mongodb.net/mahdi?retryWrites=true&w=majority";
- var databsae;
+var databsae;
 
 mongo.connect(url1,{useNewUrlParser:true,useUnifiedTopology:true},
      function(err, db) {
-    if (err) throw err;
+    if (err) throw err;   
       console.log(" conected database successfuly ...");
     
+
    
   });
   var sch={
-    name: String,
-    email:String,
-    password:Number
+    title_pro: String,
+    imag_pro:{ type: String },
+    price_cash_pro:Number,
+    price_installments_pro:Number
 }
-  const mongomodel = new mongo.model('newcols',sch);
+  const mongomodel = new mongo.model('product',sch);
+  ///
 
-//get
+  var users={
+    thing_name:String ,
+    username:String,
+    phone_number:Number,
+    address:String,
+    price:Number,
+    payment:Number,
+    remain:Number
+
+  }
+  const mongoUser=new mongo.model("users",users);
+
+
 app.get('/',(req,res)=>{
    res.send(req.body.email);
 });
-
-app.get('/data',async(req,res,next)=>{
+//get 
+app.get('/data',(req,res,next)=>{
 fetchid=req.params.id  ;
-await mongomodel.find(({id:fetchid}),(err,val)=>{
+mongomodel.find(({id:fetchid}),(err,val)=>{
 if(err) throw err;
 res.send(val);
-}) 
+})
+  
 });
+
+////////////////get item one from page view 
+app.get('/data/:id',(req,res,next)=>{
+
+  mongomodel.findById(req.params.id)
+  .then(
+    (mongomodel)=>{
+      if(!mongomodel){return res.status(404).send();}
+      res.send(mongomodel);
+    }
+  )
+  .catch((err)=>{res.status(500).send(err);});
+});
+
 
 //post
 app.post('/post',async(req,res)=>{
   console.log("inside post function ");
 
   const data=new mongomodel({
-    name:req.body.name,
-    email:req.body.email,
-    password:req.body.password
+    title_pro:req.body.title_pro,
+    imag_pro:req.body.imag_pro,
+    price_cash_pro:req.body.price_cash_pro,
+    price_installments_pro:req.body.price_installments_pro
   });
   const value =await data.save();
   res.json(value);
   console.log(req.body);
 });
+
+///////////////another get from users
+app.get('/data_users',(req,res,next)=>{
+  fetchid=req.params.id  ;
+  mongoUser.find(({id:fetchid}),(err,val)=>{
+  if(err) throw err;
+  res.send(val);
+  })
+    
+  });
+
+////////////////get one user after press at name user
+
+app.get('/information_user/:id',(req,res,next)=>{
+
+  mongoUser.findById(req.params.id)
+  .then(
+    (mongoUser)=>{
+      if(!mongoUser){return res.status(404).send();}
+      res.send(mongoUser);
+    }
+  )
+  .catch((err)=>{res.status(500).send(err);});
+});
+/////////search user by name
+app.get('/info_user/:username',(req,res,next)=>{
+
+  mongoUser.find({username:req.params.username})
+  .then(
+    (mongoUser)=>{
+      if(!mongoUser){return res.status(404).send();}
+      res.send(mongoUser);
+    }
+  )
+  .catch((err)=>{res.status(500).send(err);});
+});
+
+////////////another post to users
+
+app.post('/post_users',async(req,res)=>{
+  console.log("inside post function ");
+
+  const data=new mongoUser({
+    thing_name:req.body.thing_name,
+    username:req.body.username,
+    phone_number:req.body.phone_number,
+    address:req.body.address,
+    price:req.body.price,
+    payment:req.body.payment,
+    remain:req.body.remain
+  });
+  const value =await data.save();
+  res.json(value);
+  console.log(req.body);
+});
+//////update price for user 
+app.put('/update_price_user/:id',async(req,res,next)=>{
+  const ID=req.params.id;
+  const newData={
+    thing_name:req.body.thing_name,
+    username:req.body.username,
+    phone_number:req.body.phone_number,
+    address:req.body.address,
+    price:req.body.price,
+    payment:req.body.payment,
+    remain:req.body.remain
+}
+const result=await mongoUser.findOneAndReplace({_id:ID}
+  ,newData);
+console.log(result);
+res.json({updatedCount : result.modifiedCount});
+//res.send(req.body);
+
+}
+);
+
 //delete
 app.delete('/delete/:id',async(req,res)=>{
   
@@ -66,9 +173,10 @@ app.delete('/delete/:id',async(req,res)=>{
 app.put('/update/:id',async(req,res,next)=>{
 const ID=req.params.id;
 const newData={
-  name:req.body.name,
-    email:req.body.email,
-    password:req.body.password
+    title_pro:req.body.title_pro,
+    imag_pro:req.body.imag_pro,
+    price_cash_pro:req.body.price_cash_pro,
+    price_installments_pro:req.body.price_installments_pro
 }
 const result=await mongomodel.findOneAndReplace({_id:ID},newData);
 console.log(result);
